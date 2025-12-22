a6c0cabb55c79df85588062beaa7975484957580
Mena <dayjacs03@gmail.com>
2025-12-22 14:10:08 -0600

    server added

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const server = express();
+const path = require("node:path");
+const port = process.env.PORT || 5555;
+server.use(express.urlencoded({ extended: true }));
+
+server.set("view engine", "ejs");
+server.set("views", path.join(__dirname, "views"));
+
+server.listen(port, (error) => {
+  if (error) {
+    console.log(`whomp big ${error}`);
+  }
+  console.log("running");
+});
