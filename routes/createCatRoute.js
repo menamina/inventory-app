e1c7b9f11547a5fb909cb873c361aa5a2a5c0e23
Mena <dayjacs03@gmail.com>
2025-12-23 11:00:18 -0600

    creating separate files for creating cat + creating prods

--- routes/createCatRoute.js
+++ routes/createCatRoute.js
@@ -2,4 +2,7 @@ const mainController = require("../controls/mainController");
 const express = require("express");
 const router = express.Router();
 
+router.get("/createCategory", mainController.getCreateCategory);
+router.get("/createProduct", mainController.getCreateProduct);
+
 module.exports = router;
--- routes/createProdRoute.js
+++ routes/createProdRoute.js
@@ -2,4 +2,6 @@ const mainController = require("../controls/mainController");
 const express = require("express");
 const router = express.Router();
 
+router.get("/createProduct", mainController.getCreateProduct);
+
 module.exports = router;
