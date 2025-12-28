4acbf642974ab4c8bb95250a25808e2441472b79
Mena <dayjacs03@gmail.com>
2025-12-28 15:37:19 -0600

    connecting avien to koyeb

--- database/pool.js
+++ database/pool.js
@@ -4,6 +4,7 @@ const { Pool } = require("pg");
 // Shared connection pool for database queries
 const pool = new Pool({
   connectionString: process.env.DB_URL,
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
 });
 
 module.exports = pool;
